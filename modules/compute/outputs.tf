0ce39bc1202a7cffcd101d5ab67edb9b4566c3a6
diegombtavares <diego.mborges08@gmail.com>
2024-05-13 15:54:03 -0300

    feat: altera informações do output

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,13 +1,7 @@
-output "created-instance" {
-    value = var.client
+output "instance" {
+    value = {
+        id   = oci_core_instance.oci-instance.id
+        name = oci_core_instance.oci-instance.display_name
+        ip   = oci_core_instance.oci-instance.public_ip
+    }
 }
-
-# output "instance" {
-#     value = {
-#         id = oci_core_instance.ubuntu.id
-#         name = oci_core_instance.ubuntu.instance_name
-#     }
-  
-# }
-
-# module.<>.instance.ipv4
