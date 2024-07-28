abf3a9980d05d3fb04314735a00bf215d2fab752
Diego Tavares <diego.mborges08@gmail.com>
2024-07-28 14:57:17 -0300

    first commit

--- modules/compute/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance" {
-    value = {
-        id   = oci_core_instance.oci-instance.id
-        name = oci_core_instance.oci-instance.display_name
-        ip   = oci_core_instance.oci-instance.public_ip
-    }
-}
